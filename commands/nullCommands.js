bb49922bda1014666b869ab710bc5085f4135cd6
strelok-js <mr.strelok3000@mail.ru>
2022-07-18 21:45:20 +0300

    bat update

--- commands/nullCommands.js
+++ commands/nullCommands.js
@@ -1,5 +1,5 @@
 module.exports = async (bot,message,args,argsF) => {
-    //args - это массив аргументов. Например g/ping 0 1 2 test | args[0]="0", args[1]="1", args[2]="2", args[3]="test"
+    //args - это массив аргументов. Например g/ping 0 1 2 tEsT | args[0]="0", args[1]="1", args[2]="2", args[3]="test" . А так-же argsF[3]=="tEsT"
     return message.reply({ //Это команда пинг!
         content: "pong!"
     });
---
+++ installPackage.bat
@@ -0,0 +1,2 @@
+npm install discord.js@4.0.1 fs simple-discord.db
+pause
